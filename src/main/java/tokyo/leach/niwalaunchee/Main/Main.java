bd60d28505adff3d55bdc6b2de5fcd895cb41eb4
niwatolli3 <niwatolli3@gmail.com>
2016-09-05 22:29:30 +0900

    refs #7 @20m * add main files

---
+++ src/main/java/tokyo/leach/niwalaunchee/Main/Main.java
@@ -0,0 +1,14 @@
+package tokyo.leach.niwalaunchee.Main;
+
+/**
+ * Created by tomi on 2016/09/05.
+ */
+public class Main {
+	/**
+	 * Start point
+	 * @param args
+	 */
+	public static void Main(String[] args) {
+
+	}
+}
