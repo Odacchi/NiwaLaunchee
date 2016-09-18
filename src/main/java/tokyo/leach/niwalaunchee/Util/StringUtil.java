9de321619518e179cf07cefc9904c337be9d3209
Odacchi <lucied2007@gmail.com>
2016-09-19 03:19:56 +0900

    Update StringUtil.java
    I tried to review the javadoc of "StringUtil.java".

--- src/main/java/tokyo/leach/niwalaunchee/Util/StringUtil.java
+++ src/main/java/tokyo/leach/niwalaunchee/Util/StringUtil.java
@@ -5,11 +5,12 @@ package tokyo.leach.niwalaunchee.Util;
  */
 public class StringUtil {
 	/**
-	 * Split string left and right by char(String ch)
+	 * Split string left and right by char(String ch).
+	 * <p>
 	 * ex) string: "g today's weather" -> ch: " ", left: g, right: today's weather
-	 * @param str
-	 * @param ch
-	 * @return
+	 * @param str the target string. not {@code null}
+	 * @param ch the delimiting character. not {@code null}
+	 * @return the array of 2 strings. Returns an string array which contains one empty character
 	 */
 	public static String[] splitLRByChar(String str, String ch) {
 		return str.split(ch, 2);
